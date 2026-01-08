3e868a8d3d7cf75e06d05241350bf48da4fa8326
Lucas Brizola <tyfvf2@gmail.com>
2026-01-08 00:04:17 -0300

    feat: modified to play happy birthday

--- main.c
+++ main.c
@@ -12,15 +12,11 @@ typedef float f32;
 void write_u16_le(u16 value, FILE *file) {
   fputc(value & 0xFF, file); // LSB
   fputc((value >> 8) & 0xFF, file); // MSB
-
-  // fwrite(&value, sizeof(u16), 1, file);
 }
 
 void write_i16_le(i16 value, FILE *file) {
   fputc(value & 0xFF, file); // LSB
   fputc((value >> 8) & 0xFF, file); // MSB
-
-  // fwrite(&value, sizeof(i16), 1, file);
 }
 
 void write_u32_le(u32 value, FILE *file){
@@ -28,22 +24,37 @@ void write_u32_le(u32 value, FILE *file){
   fputc((value >> 8) & 0xFF, file); // Byte 1
   fputc((value >> 16) & 0xFF, file); // Byte 2
   fputc((value >> 24) & 0xFF, file); // Byte 3
-
-  // fwrite(&value, sizeof(u32), 1, file);
 }
+
+typedef struct {
+  f32 freq;
+  f32 duration;
+} Note;
+
 
 #define WRITE_STR_LIT(str, file) fwrite((str), 1, sizeof(str) - 1, (file))
 
 #define FREQ 44100
-#define DURATION 3
 #define NUM_CHANNELS 1
 #define BITS_PER_SAMPLE 16
 
 int main(void) {
+  Note happy_birthday[] = {
+    {392.00f, 0.5f}, {392.00f, 0.5f}, {440.00f, 1.0f}, {392.00f, 1.0f}, {523.25f, 1.0f}, {493.88f, 2.0f},
+    {392.00f, 0.5f}, {392.00f, 0.5f}, {440.00f, 1.0f}, {392.00f, 1.0f}, {587.33f, 1.0f}, {523.25f, 2.0f},
+    {392.00f, 0.5f}, {392.00f, 0.5f}, {783.99f, 1.0f}, {659.25f, 1.0f}, {523.25f, 1.0f}, {493.88f, 1.0f}, {440.00f, 2.0f},
+    {698.46f, 0.5f}, {698.46f, 0.5f}, {659.25f, 1.0f}, {523.25f, 1.0f}, {587.33f, 1.0f}, {523.25f, 2.0f}
+  };
+
   FILE *file = fopen("output.wav", "wb");
   
-  u32 num_samples = FREQ * DURATION;
-  u32 file_size = num_samples * sizeof(u16) + 44; // 44 bytes for WAV header
+  u32 total_samples = 0;
+  for (u32 i = 0; i < sizeof(happy_birthday)/sizeof(happy_birthday[0]); i++) {
+    total_samples += (u32)(happy_birthday[i].duration * FREQ);
+  }
+
+  u32 data_size = total_samples * sizeof(u16);
+  u32 file_size = data_size + 44; // 44 bytes for WAV header
 
   // Write WAV header
   WRITE_STR_LIT("RIFF", file);
@@ -60,16 +71,21 @@ int main(void) {
   write_u16_le(BITS_PER_SAMPLE, file); // BitsPerSample
 
   WRITE_STR_LIT("data", file);
-  write_u32_le(num_samples * sizeof(u16), file); // Subchunk2Size
-
+  write_u32_le(data_size, file); // Subchunk2Size
+  
   // Write audio data
-  for (u32 i = 0; i < num_samples; i++) {
-    f32 t = (f32)i / FREQ;
+  for (u32 i = 0; i < sizeof(happy_birthday)/sizeof(happy_birthday[0]); i++) {
+    Note note = happy_birthday[i];
+    u32 samples = (u32)(note.duration * FREQ);
 
-    f32 sample = 0.25f * sinf(2.0f * 3.14159265f * 440.0f * t); // 440 Hz sine wave
-    i16 int_sample = (i16)(sample * INT16_MAX); // Scale to int16 range
+    for (u32 j = 0; j < samples; j++) {
+      f32 t = (f32)j / FREQ;
+      f32 sample = 0.25f * sinf(2.0f * 3.14159265f * note.freq * t);
+      i16 int_sample = (i16)(sample * INT16_MAX); // Scale to int16 range
 
-    write_i16_le(int_sample, file);
+      write_i16_le(int_sample, file);
+    }
+
   }
 
   fclose(file);
